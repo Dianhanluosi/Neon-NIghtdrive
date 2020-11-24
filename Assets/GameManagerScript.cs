08626679361d9119e55c421f7e1aea38aeb4a3b7
Dianhanluosi <evanzheng1127@gmail.com>
2020-11-24 08:43:47 -0800

    Scenemanager scripts first commit 843AM 11/24/2020

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static GameManagerScript _instance;
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(this);
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    SceneLoaderScript sceneloader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneloader = GetComponent<SceneLoaderScript>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sceneloader.LoadMainScene("StartScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sceneloader.LoadMainScene("MainScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sceneloader.LoadMainScene("EndScene");
+        }
+    }
+}
