0868e58d05b7b4f7db6c2f482648d227199fdab0
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-08 00:57:11 -0800

    Sun rotation controller 057AM 12/8/2020

---
+++ Assets/Sun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private int degree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        gameObject.transform.Rotate(speed * Time.deltaTime, 0, 0);
+    }
+}
