1452e5d9984dda5e22c07374effcf820ab7ab173
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 01:42:56 -0800

    Steering wheel script complete 141AM 12/13/2020

---
+++ Assets/SteeringWheelRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheelRotate : MonoBehaviour
+{
+    public CC wheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(20, 90, 0);
+        if (wheel.left && !wheel.atleft)
+        {
+
+            transform.rotation = Quaternion.Euler(20, 90, 20);
+        }
+        if (wheel.right && !wheel.atright)
+        {
+            transform.rotation = Quaternion.Euler(20, 90, -20);
+
+        }
+    }
+}
