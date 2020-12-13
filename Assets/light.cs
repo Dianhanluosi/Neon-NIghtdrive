72f909570500587be292ec944ab7447e3f71e856
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 00:44:29 -0800

    light switch script complete 043AM 12/13/2020

---
+++ Assets/light.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    public CC cscript;
+    public Light l;
+    
+    void Start()
+    {
+        //cscript = GetComponent<Car.CC>();
+        l = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        l.enabled = false;
+        if (cscript.engine)
+        {
+            l.enabled = true;
+        }
+       
+
+    }
+}
