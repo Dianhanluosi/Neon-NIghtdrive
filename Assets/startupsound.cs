e5a9b294ca738498b60791e05973813f59254f37
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 00:45:39 -0800

    engine starup sounds script complete 043AM 12/13/2020

---
+++ Assets/startupsound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startupsound : MonoBehaviour
+{
+    public CC cac;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!cac.started &&  cac.engine)
+        {
+            if (cac.clutchin && cac.ignition)
+            {
+                startup();
+            }
+
+        }
+    }
+
+    void startup()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+            cac.started = true;
+        }
+       
+    }
+
+}
