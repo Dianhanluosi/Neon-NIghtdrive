eb111218b85e655fa064b6a67e66721b79932769
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 01:42:41 -0800

    Cam switch script complete 141AM 12/13/2020

---
+++ Assets/CamSwitch.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject camera3;
+    public GameObject camera4;
+    public GameObject camera5;
+
+    public int cam = 1;
+
+    void Start()
+    {
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+        camera3.SetActive(false);
+        camera4.SetActive(false);
+        camera5.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cam += 1;
+        }
+        if (cam == 6)
+        {
+            cam = 1;
+        }
+
+        if (cam == 1)
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (cam == 2)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (cam == 3)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(true);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (cam == 4)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(true);
+            camera5.SetActive(false);
+
+
+        }
+        if (cam == 5)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(true);
+
+
+        }
+    }
+}
