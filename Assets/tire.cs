c4c2512ef45f355ec3b9d376fb124f883d6b2912
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 00:44:51 -0800

    Tire rotation script complete 043AM 12/13/2020

---
+++ Assets/tire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tire : MonoBehaviour
+{
+    public CC ct;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(ct.tire, 0, 0);
+    }
+}
