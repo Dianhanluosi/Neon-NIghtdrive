dcea039a7ee293c0cb2c8c2dad73c56a93665137
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 03:49:13 -0800

    CamSwitch changed to automatic 349AM 12/13/2020

--- Assets/CamSwitch.cs
+++ Assets/CamSwitch.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CamSwitch : MonoBehaviour
 {
+    public CC start;
     public GameObject camera1;
     public GameObject camera2;
     public GameObject camera3;
@@ -11,6 +12,8 @@ public class CamSwitch : MonoBehaviour
     public GameObject camera5;
 
     public int cam = 1;
+
+    
 
     void Start()
     {
@@ -21,8 +24,20 @@ public class CamSwitch : MonoBehaviour
         camera5.SetActive(false);
     }
 
+    public float timer = 150f;
+
     void Update()
     {
+        if (start.started)
+        {
+            timer -= Time.deltaTime;
+
+        }
+        else
+        {
+           
+        }
+
 
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -33,7 +48,61 @@ public class CamSwitch : MonoBehaviour
             cam = 1;
         }
 
-        if (cam == 1)
+        if (timer <= 150 && timer > 120)
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (timer <= 120 && timer > 90)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (timer <= 90 && timer > 60)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(true);
+            camera4.SetActive(false);
+            camera5.SetActive(false);
+
+        }
+        if (timer <= 60 && timer > 30)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(true);
+            camera5.SetActive(false);
+
+
+        }
+        if (timer <= 30 && timer > 0)
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+            camera5.SetActive(true);
+
+
+        }
+        if (timer <= 0f)
+        {
+            timer = 150f;
+
+
+        }
+
+        /*if (cam == 1)
         {
             camera1.SetActive(true);
             camera2.SetActive(false);
@@ -79,6 +148,6 @@ public class CamSwitch : MonoBehaviour
             camera5.SetActive(true);
 
 
-        }
+        }*/
     }
 }
