66a73914992f77491ef060e6c2771b99b6cb351b
Dianhanluosi <evanzheng1127@gmail.com>
2020-12-13 18:47:00 -0800

    new assets  1845PM 12/13/2020

---
+++ Assets/textcolorclicked.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textcolorclicked : MonoBehaviour
+{
+    public Text t;
+    // Start is called before the first frame update
+    
+
+    public void ChangeColor()
+    {
+        t.color = Color.yellow;
+    }
+
+
+}
---
+++ Assets/textcolorhover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class textcolorhover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text theText;
+
+
+
+    void Start()
+    {
+        theText.color = Color.red;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.white; //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.red ; //Or however you do your color
+    }
+
+}
